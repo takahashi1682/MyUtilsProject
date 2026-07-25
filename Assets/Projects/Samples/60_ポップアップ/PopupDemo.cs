b24941bad2756a3afdd8f889c22a5abf48cc3448
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2026-07-25 16:58:45 +0900

    Basic追加

---
+++ Assets/Projects/Samples/60_ポップアップ/PopupDemo.cs
@@ -0,0 +1,15 @@
+using MyUtils.PopupWindow;
+using UnityEngine;
+
+namespace Projects._60_ポップアップ
+{
+    public class PopupDemo : MonoBehaviour
+    {
+        public async void OnShowPopup()
+        {
+            var popup = await PopupPanel.WaitInstanceAsync;
+            int answer = await popup.ShowPopupPanel("Do you Playing Game?");
+            Debug.Log(answer);
+        }
+    }
+}

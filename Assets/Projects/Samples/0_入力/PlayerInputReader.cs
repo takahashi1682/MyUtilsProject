772570f032f7e4cc0cbb618f4146eed0eba52456
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2026-07-31 08:04:02 +0900

    Input

--- Assets/Projects/Samples/0_入力/PlayerInputReader.cs
+++ Assets/Projects/Samples/0_入力/PlayerInputReader.cs
@@ -8,9 +8,14 @@ namespace Projects._0_入力
     ///  Input System の入力を受け取る ScriptableObject
     ///  ScriptableObjectなので、一度ロードされるとアプリ終了まで存在し続けます
     /// </summary>
-    [CreateAssetMenu(fileName = "InputReader", menuName = "Input/Input Reader")]
-    public class PlayerInputReader : ScriptableObject, InputSystem_Actions.IPlayerActions
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Input System/Input Reader")]
+    public class PlayerInputReader : AbstractInputReader
     {
+        [SerializeField] private InputActionReference _moveAction;
+        [SerializeField] private InputActionReference _lookAction;
+        [SerializeField] private InputActionReference _fireAction;
+        [SerializeField] private InputActionReference _mousePositionAction;
+
         public ReadOnlyReactiveProperty<Vector2> Move => _move;
         private readonly ReactiveProperty<Vector2> _move = new();
 
@@ -23,52 +28,36 @@ namespace Projects._0_入力
         public ReadOnlyReactiveProperty<Vector2> MousePosition => _mousePosition;
         private readonly ReactiveProperty<Vector2> _mousePosition = new();
 
-        private InputSystem_Actions _instance;
-
-        public void EnablePlayerInput() => _instance?.Player.Enable();
-        public void DisablePlayerInput() => _instance?.Player.Disable();
-        public void EnableUIInput() => _instance?.UI.Enable();
-        public void DisableUIInput() => _instance?.UI.Disable();
-
-        private void OnEnable()
+        protected override void OnEnable()
         {
-            if (_instance == null)
-            {
-                _instance = new InputSystem_Actions();
-                _instance.Player.SetCallbacks(this);
-            }
+            base.OnEnable();
 
-            EnablePlayerInput();
+            RegisterValueAction(_moveAction, OnMove);
+            RegisterValueAction(_lookAction, OnLook);
+            RegisterValueAction(_mousePositionAction, OnMousePosition);
+            RegisterButtonAction(_fireAction, OnFire);
         }
 
-        private void OnDisable()
+        protected override void OnDisable()
         {
-            if (_instance == null) return;
+            UnregisterValueAction(_moveAction, OnMove);
+            UnregisterValueAction(_lookAction, OnLook);
+            UnregisterValueAction(_mousePositionAction, OnMousePosition);
+            UnregisterButtonAction(_fireAction, OnFire);
 
-            _instance.Player.Disable();
-            _instance.UI.Disable();
+            base.OnDisable();
         }
 
-        private void OnDestroy()
-        {
-            if (_instance == null) return;
-
-            _instance.Player.SetCallbacks(null);
-            _instance.Disable();
-            _instance.Dispose();
-            _instance = null;
-        }
-
-        public void OnMove(InputAction.CallbackContext context)
+        private void OnMove(InputAction.CallbackContext context)
             => _move.Value = context.ReadValue<Vector2>();
 
-        public void OnLook(InputAction.CallbackContext context)
+        private void OnLook(InputAction.CallbackContext context)
             => _look.Value = context.ReadValue<Vector2>();
 
-        public void OnFire(InputAction.CallbackContext context)
+        private void OnFire(InputAction.CallbackContext context)
             => _fire.Value = context.ReadValueAsButton();
 
-        public void OnMousePosition(InputAction.CallbackContext context)
+        private void OnMousePosition(InputAction.CallbackContext context)
             => _mousePosition.Value = context.ReadValue<Vector2>();
     }
 }
